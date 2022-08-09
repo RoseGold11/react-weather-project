914fda626ab046dc6a7426ff4d38bcbc0a2892e6
Rachael Moten <motenrachael@gmail.com>
2022-08-09 14:11:00 -0400

    Added Github repository and personal website URL

--- src/App.js
+++ src/App.js
@@ -2,5 +2,25 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://clever-almeida-63b7f4.netlify.app/index.html"
+          target="_blank"
+        >
+          Rachael Moten
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/RoseGold11/react-weather-project"
+          target="_blank"
+        >
+          open-sourced on Github.
+        </a>
+      </footer>
+    </div>
+  );
 }
