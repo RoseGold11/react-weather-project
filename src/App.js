4898d9adbd14f7e2d73699fae5478dc61e0b5be5
Rachael Moten <motenrachael@gmail.com>
2022-08-09 14:38:21 -0400

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,23 +4,25 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://clever-almeida-63b7f4.netlify.app/index.html"
-          target="_blank"
-        >
-          Rachael Moten
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/RoseGold11/react-weather-project"
-          target="_blank"
-        >
-          open-sourced on Github.
-        </a>
-      </footer>
+      <div claaName="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://clever-almeida-63b7f4.netlify.app/index.html"
+            target="_blank"
+          >
+            Rachael Moten
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/RoseGold11/react-weather-project"
+            target="_blank"
+          >
+            open-sourced on Github.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
