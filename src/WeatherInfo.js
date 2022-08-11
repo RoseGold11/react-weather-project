cd48da8e3a198e579425841ef3ac737b6e8240f6
Rachael Moten <motenrachael@gmail.com>
2022-08-10 20:03:56 -0400

    changed default city

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -29,7 +29,7 @@ export default function WeatherInfo(props) {
         </div>
         <div className="col-6">
           <ul>
-            <li>Feels like {Math.round(props.data.feels)}°</li>
+            <li>Feels like {Math.round(props.data.feels)}°F </li>
             <li>Humidity: {props.data.humidity}%</li>
             <li>Wind: {Math.round(props.data.wind)}km/h</li>
           </ul>
